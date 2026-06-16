4665a1293968a47ae2e4ccc97589d118c3f51a9f
ToanPK <phamkhanhtoan217@gmail.com>
2026-06-16 22:49:25 +0700

    project structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-5xl font-bold text-blue-600">
-        DMM thuan
+        DMM thuann
       </h1>
     </div>
   )
